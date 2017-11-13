32e45c0b8184b0e312e34d10722c28b2663fba65
Millie Hilgart <e5t2oaah@gmail.com>
2017-11-13 07:30:42 +0200

    Final comments

--- ProvinceTable/app.R
+++ ProvinceTable/app.R
@@ -1,12 +1,12 @@
 ## Province Data Table
 
 # The packages
-library(shiny)
-library(DT)
-library(dplyr)
+library(shiny)          # Generates shiny app
+library(DT)             # Generates dynamic tables
+library(dplyr)          # Data manipulation 
 
 # Path to data table
-df = read.csv(file = "SDG_Millie_vb_edit_provinces_v2.csv", sep=",") # Original table
+df = read.csv(file = "SDG_Millie_vb_edit_provinces_v2_SuCor.csv", sep=",") # Original table
 
 SDG = df[,colSums(is.na(df)) != nrow(df)] # Remove empty columns
 
@@ -15,8 +15,8 @@ SDGprov <- SDG[1:4112,] %>%
         mutate(Level = ifelse(Province == "All Indonesia", # Generate a column to distinguish national and provincial data
                               "National", 
                               "Provincial"))%>%
-        filter(Level == "Provincial")%>%
-        select("SDG" = "SDG.Goal", 
+        filter(Level == "Provincial")%>% # Subset out rows with provincial data
+        select("SDG" = "SDG.Goal", # Select columns with provincial data
                "Indicator", 
                "Value", 
                "Province", 
@@ -33,17 +33,16 @@ ui <- navbarPage(theme = "bootstrap.css",
                  tabPanel(
                          # SDG Input
                          selectInput("select1", # Reactive input name
-                                     "Select a Sustainable Devlopment Goal/s:", # Input caption
-                                     choices = c("Goal 1: No Poverty" = 1, # List of input values
+                                     "Select One or More Sustainable Devlopment Goals:", # Input caption
+                                     choices = c("Goal 1: No Poverty" = 1, # Make the SDG numbers appear with their titles in the UI
                                                  "Goal 2: Zero Hunger" = 2, 
                                                  "Goal 3: Good Health and Well-Being" = 3, 
                                                  "Goal 4 : Quality Education" = 4, 
                                                  "Goal 5: Gender Equality" = 5, 
                                                  "Goal 6: Clean Water and Sanitation" = 6, 
-                                                 "Goal 8: Decent Work and Economic Growth" = 8, 
                                                  "Goal 13: Climate Action" = 13, 
                                                  "Goal 16: Peace Justice and Strong Institutions" = 16), 
-                                     selected = "Goal 1: No Poverty", # Pre-selected input for UI
+                                     selected = "Goal 1: No Poverty", # Pre-selected input for UI- this does not work with multiple = TRUE
                                      multiple = TRUE) # Can the user select multiple options
                  ),
                  tabPanel(
@@ -54,10 +53,16 @@ ui <- navbarPage(theme = "bootstrap.css",
                  # The rest of UI will fall below the tab panel 
                  
                  # Table output
-                 DT::dataTableOutput("SDGtable1"),
-                 
+                 DT::dataTableOutput("SDGtable1"), # Reactive table name
+
                  # Button
-                 downloadButton("downloadData", "Download")
+                 downloadButton("downloadData", "Download"),
+                                  
+                 # Message
+                 wellPanel( # Box below the table with key information 
+                         h5("Unfortunately, no provincial level data is available for Goal 8: Decent Work and Economic Growth.")
+                 )
+
 )        
 
 server <- function(input, output){
@@ -68,7 +73,7 @@ server <- function(input, output){
                 selectInput("select2", # Reactive input name
                             "Select a Province:", # Title for dropdown 
                             choices = choice, # Indicator options for the selected SDG
-                            selected = choice[1]
+                            selected = choice[1] # Preselect the first option 
                 )
         })
         
@@ -84,31 +89,27 @@ server <- function(input, output){
         
         # Generate data table 
         output$SDGtable1 <- DT::renderDataTable({
-                DT::datatable(SDGselect(),
+                DT::datatable(SDGselect(), # Reactive table 
                               extensions = c( # Extensions allow for different things to occur in the UI, has to be matched with compatible 'options' in order to appear 
                                       'ColReorder', # Allows col to be reordered 
-                                      'Buttons', # Allows buttons
-                                      'Scroller'), # Allows scroller 
+                                      'Buttons'), # Allows buttons
                               options = list(
                                       dom = 'Bfrtip', # Denotes where things appear in the UI, must have for buttons to appear! 
                                       colReorder = TRUE, # Give user ability to reorder the col 
                                       buttons = c('copy', # Copy the current table 
                                                   'print', # Print table
                                                   'colvis'), # Generates button that allows the columns to appear to be selected or unselected- all columns appear in download! 
-                                      searchHighlight = TRUE, # Cause results from search filter to highlight
-                                      deferRender = FALSE, # For use with large data sets to determine when an UI action is rendered 
-                                      scrollY = 475, # Verticle scrolling, numeric value determines height of table- 475 displays 13 row with the current CSS  
-                                      scroller = TRUE)) # Should the scroller appear
+                                      searchHighlight = TRUE)) # Cause results from search filter to highlight
         })
         
         # Downloadable csv of selected dataset ----
         output$downloadData <- downloadHandler(
-                filename = function() {
+                filename = function() { # Generate a .csv file
                         paste(input$dataset, "SDGprov.csv", sep = ",")
-                }, # Generate a .csv file
-                content = function(file) {
+                }, 
+                content = function(file) { # Write .csv to a particular directory
                         write.csv(SDGselect(), file, na="")
-                }) # Write .csv to a particular directory 
+                })  
                
 }
-shinyApp(ui, server)  
+shinyApp(ui, server) # Generate app  
