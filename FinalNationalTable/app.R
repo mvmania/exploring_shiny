1aa8f02eb1408556ac2ccb621fa76c670e61b3d3
Millie Hilgart <e5t2oaah@gmail.com>
2017-11-13 07:10:29 +0200

    Final comments

--- FinalNationalTable/app.R
+++ FinalNationalTable/app.R
@@ -3,35 +3,51 @@
 # The packages
 library(shiny)          # Generates shiny app
 library(DT)             # Generates dynamic tables
-library(ggplot2)        # Geberates plots
 library(dplyr)          # Data manipulation 
 
 # Data Editing 
 
 # Path to data
-df = read.csv(file = "SDG_Millie_vb_edit.csv", sep=",")
+df = read.csv(file = "SDG_Millie_vb_edit_provinces_v2_SuCor.csv", sep=",")
 
-#Remove empty columns from dataset 
-SDG = df[,colSums(is.na(df)) != nrow(df)]
+SDG = df[,colSums(is.na(df)) != nrow(df)] # Remove empty columns from dataset
 
-SDGdf1 <- SDG[1:4112,] %>%
-        mutate(Level = ifelse(Province == "All Indonesia", "National", 
-                              "Provincial"))
-SDGdf2 <- SDGdf1 %>%
-        filter(Level == "National")
+SDGdf1 <- SDG[1:4112,] %>% 
+        mutate(Level = ifelse(Province == "All Indonesia", # Create a new variable that describes if data is National or Provincial level data 
+                              "National", 
+                              "Provincial")) %>%
+        mutate(Quintile = ifelse(# Generate a column with wealth categories to match those found in the report on page 25
+                Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest. == "Q5 (wealthiest)", "Richest 20%", 
+                ifelse(Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest. == "Q1 (poorest)", "Poorest 20%", 
+                       ifelse(Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest. == "Q2", "Second 20%", 
+                              ifelse(Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest. == "Q3", "Middle 20%", 
+                                     ifelse(Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest. == "Q4", "Fourth 20%", "All"))))))%>%
+        mutate(Ed = ifelse(# Generate a column with education categories to match those found in the report on page 25
+                Educational.attainment.of.household.head == "No school", "None", 
+                ifelse(Educational.attainment.of.household.head == "Incomplete primary school (SD)/Islamic primary school (MI)", "Some Primary",
+                       ifelse(Educational.attainment.of.household.head == "Incomplete primary school", "Some Primary",
+                              ifelse(Educational.attainment.of.household.head == "Graduated primary school", "Primary",
+                                     ifelse(Educational.attainment.of.household.head == "Graduated primary school (SD/MI)", "Primary", 
+                                            ifelse(Educational.attainment.of.household.head == "Some secondary school", "Some Secondary",
+                                                   ifelse(Educational.attainment.of.household.head == "Graduated junior secondary school (SMP/MTS)", "Junior Secondary",
+                                                          ifelse(Educational.attainment.of.household.head == "Graduated from secondary school", "Secondary", 
+                                                                 ifelse(Educational.attainment.of.household.head == "Graduated senior secondary school (SMA/MA)", "Senior Secondary",     
+                                                                        ifelse(Educational.attainment.of.household.head == "More than secondary school", "Some Tertiary",
+                                                                               ifelse(Educational.attainment.of.household.head == "Graduated university (D1-D3/PT)", "Tertiary", "All")))))))))))) 
 
-SDGdf <- SDGdf2 %>%
-        select("SDG" = "SDG.Goal", 
-               "Indicator" = "Indicator", 
-               "Value" = "Value",
-               "Gender" = "Gender..All.Male.Female.", 
-               "Geography" = "Geography..All.Urban.Rural.", 
+SDGdf <- SDGdf1 %>% # Subset the National data only
+        filter(Level == "National")%>%
+        select("SDG" = "SDG.Goal", # Select the variables that apply to the National data
+               "Indicator", 
+               "Value", 
+               "Sex" = "Gender..All.Male.Female.", 
+               "Residence" = "Geography..All.Urban.Rural.", 
                "Disability" = "Disability..All.With.Without.", 
-               "WealthQuintile" = "Wealth.Quintile..All.Poorest.Second.Middle.Fourth.Richest.", 
-               "Education" = "Educational.Attainment.of.Household.Head", 
+               "WealthQuintile" = "Quintile", 
+               "Education" = "Ed", 
                "Age" = "Age.Range..i.e..0.24.M..15.60.Y.", 
-               "Year" = "Year", 
-               "Source" = "Source")
+               "Year", 
+               "Source")
 
 # The app
 ui <- fluidPage(theme = "bootstrap.css",
@@ -44,10 +60,10 @@ ui <- fluidPage(theme = "bootstrap.css",
                         # Sidebar panel for inputs ----
                         sidebarPanel(
                                 # SDG Input
-                                selectInput("select1", 
-                                            "Select Sustainable Development Goal:", 
+                                selectInput("select1", # Input 
+                                            "Select Sustainable Development Goal:", # Input Title 
                                             choices = c(
-                                                    "Goal 1: No Poverty" = 1, 
+                                                    "Goal 1: No Poverty" = 1, # Make the SDG numbers appear with their titles in the UI
                                                     "Goal 2: Zero Hunger" = 2, 
                                                     "Goal 3: Good Health and Well-Being" = 3, 
                                                     "Goal 4 : Quality Education" = 4, 
@@ -91,12 +107,12 @@ server <- function(input, output){
         # Generate the checkbox for Indicators
         output$checkbox1 <- renderUI({
                 
-                choice <-  unique(SDGdf[SDGdf$SDG %in% input$select1, "Indicator"])
+                choice <-  unique(SDGdf[SDGdf$SDG %in% input$select1, "Indicator"]) # For the selected SDG, subset the Indicators
                 
-                checkboxGroupInput("checkbox1",
-                                   "Select Indicator:", 
-                                   choices = choice, 
-                                   selected = choice[1])
+                checkboxGroupInput("checkbox1", # Reactive input
+                                   "Select Indicator:", # Input title 
+                                   choices = choice, # What names go into the input list
+                                   selected = choice[1]) # When the table comes up, the first option in the list should be selected
         })
         
         # Generate data frame
@@ -112,21 +128,31 @@ server <- function(input, output){
                 
                 oo <- df() # Variable for reactive data
                 
-                oo$Value <- as.numeric(as.character(oo$Value)) # Coerce Value into numeric data type
+                oo$Value <- as.numeric(as.character(oo$Value)) # Coerce Value column into numeric data type
                 
                 # Generate data table 
                 output$SDGtable1 <- DT::renderDataTable({
-                        DT::datatable(oo)
+                        DT::datatable(oo, # Reactive table input
+                        extensions = c( # Extensions allow for different things to occur in the UI, has to be matched with compatible 'options' in order to appear 
+                                'ColReorder', # Allows col to be reordered 
+                                'Buttons'), # Allows buttons 
+                        options = list(
+                                dom = 'Bfrtip', # Denotes where things appear in the UI, must have for buttons to appear! 
+                                colReorder = TRUE, # Give user ability to reorder the col 
+                                buttons = c('copy', # Copy the current table 
+                                            'print', # Print table
+                                            'colvis'), # Generates button that allows the columns to appear to be selected or unselected- all columns appear in download! 
+                                searchHighlight = TRUE)) # Cause results from search filter to highlight
                 })
         })
         # Downloadable csv of selected dataset ----
         output$downloadData <- downloadHandler(
-                filename = function() {
-                        paste(input$dataset, ".csv", sep = "")
-                }, # Generate a .csv file
-                content = function(file) {
-                        write.csv(datasetInput(), file, row.names = FALSE)
-                }) # Write .csv to a particular directory 
+                filename = function() { # Generate a .csv file
+                        paste(input$dataset, "SDGdf.csv", sep = ",")
+                }, 
+                content = function(file) { # Write .csv to a particular directory
+                        write.csv(df(), file, na="")
+                })  
         
 }
-shinyApp(ui, server)
+shinyApp(ui, server) # Run shiny app
